928f7829c73a8512ac0f0651542fce6a08b2015a
cxwii <1471749154@qq.com>
2023-03-10 11:41:47 +0800

    初步搭建完毕

--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
 const express = require('express')
 const app = express()
 
+// 解决跨域
 const cors = require('cors')
 app.use(cors())
+
+// 解决boyd内容为空
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ express: false }))
+app.use(bodyParser.json())
 
 app.use(express.urlencoded({ extended: false }))
 
 const loginRouter = require('./router/login')
 app.use('/api', loginRouter)
 
-app.listen(9529, () => {
+app.listen(9528, () => {
   console.log('juejimcsApi server running at http://127.0.0.1:9529 :>>')
 })
