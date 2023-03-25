501e4c915285d5eacd340f39b80ecff36a14cf3c
cxwii <1471749154@qq.com>
2023-03-25 12:42:08 +0800

    连接数据库

--- routre-handler/loginHandler.ts
+++ routre-handler/loginHandler.ts
@@ -1,14 +1,20 @@
+const myDB = require('../sql/mysql')
+
 exports.login = (req, res) => {
-  console.log('object :>> ', req.body);
-  if (req.body.userName == 'admin' && req.body.password == 123456) {
-    res.send({
-      status: 200,
-      message: '登录成功'
-    })
-  } else {
-    res.send({
-      status: 500,
-      message: '登录失败'
-    })
-  }
+  myDB.query('select * from user', (err, results) => {
+    if (err) {
+      return console.log('mysql_err :>> ', err)
+    }
+    if (req.body.userName == results[0].userName && req.body.password == results[0].password) {
+      res.send({
+        status: 200,
+        message: '登录成功'
+      })
+    } else {
+      res.send({
+        status: 500,
+        message: '登录失败'
+      })
+    }
+  })
 }
